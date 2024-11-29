7de85e3ad0877466a21f64e15c029dffd0fb4d6b
FRENTYGOD1 <151792528+FRENTYGOD1@users.noreply.github.com>
2024-11-29 05:07:38 +0000

    Update PayrollSystem.java

--- UL Payroll System/src/Core_System/PayrollSystem.java
+++ UL Payroll System/src/Core_System/PayrollSystem.java
@@ -1,28 +1,33 @@
+package core;
+
+import core.FullTimeEmployee;
+import core.Employee;
+
 import java.util.ArrayList;
 import java.util.List;
 
 //Payroll system class, responsible for managing employees and generating payslips
 public class PayrollSystem {
-    private List<Staff> staffList;
+    private List<Employee> employees;
     private List<Payslip> payslips;
 
     //Initialisation list
     public PayrollSystem(){
-        this.staffList = new ArrayList<>();
+        this.employees = new ArrayList<>();
         this.payslips = new ArrayList<>();
     }
 
     //add new staffs
-    public void addStaff(Staff staff){
-        staffList.add(staff);//add staff into list
-        System.out.println("Staff Added: " + staff.getName());//output confirm information
+    public void addStaff(Employee employee){
+        employees.add(employee);//add staff into list
+        System.out.println("core.Staff Added: " + employee.getName());//output confirm information
     }
 
     //Generate payroll for all employees
     public void generatePayslips(){
-        for (Staff staff : staffList){//Iterate through the staff list
-            double netPay = staff.calculateMonthlyPay();//calc net salary
-            Payslip payslip = new Payslip(staff.getid(), staff.getSalaryScale(), netPay);//create payslip
+        for (Employee employee : employees){//Iterate through the staff list
+            double netPay = employee.calculateMonthlyPay();//calc net salary
+            Payslip payslip = new Payslip(employee.getid(), employee.getSalaryScale(), netPay);//create payslip
             payslips.add(payslip);//add payslip to the list
             System.out.println(payslip);
         }
@@ -30,10 +35,10 @@ public class PayrollSystem {
 
     //promote full-time staff
     public void promoteFullTimeStaff(String id){//Iterate through the staff list
-        for (Staff staff : staffList){
-            if (staff instanceof FullTimeStaff && staff.getid().equals(id)){
+        for (Employee employee: employees){
+            if (employee instanceof FullTimeEmployee && employee.getid().equals(id)){
                 //Check if you are a full-time employee and match the number
-                staff.promoteToNextPoint();//promote to the next point
+                employee.promoteToNextPoint();//promote to the next point
                 break;
             }
         }
