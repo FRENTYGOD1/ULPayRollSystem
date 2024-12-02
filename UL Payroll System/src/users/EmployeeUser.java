3f4668e926acc74a5e04ca10319bfd4a0eeb7e51
FRENTYGOD1 <151792528+FRENTYGOD1@users.noreply.github.com>
2024-12-02 05:27:49 +0000

    Update EmployeeUser.java

--- UL Payroll System/src/users/EmployeeUser.java
+++ UL Payroll System/src/users/EmployeeUser.java
@@ -1,17 +1,29 @@
 package users;
 
+/**
+ * The EmployeeUser class represents an employee user in the system. It holds the employee's username
+ * and provides functionality for displaying the employee's menu options.
+ */
 public class EmployeeUser {
+    // Employee's username
     private String username;
 
+    /**
+     * Constructor to create an EmployeeUser object with a specific username.
+     *
+     * @param username The username of the employee.
+     */
     public EmployeeUser(String username) {
-        this.username = username;//Set employee's username
+        this.username = username; // Set employee's username
     }
 
-
-    //Employee User Menu
+    /**
+     * Displays the employee's menu options.
+     * This method currently provides options for viewing the payslip.
+     * Additional options can be added as needed.
+     */
     public void menu() {
         System.out.println("Employee Menu:");
-        System.out.println("Viewing Payslip...");//Export View Payroll Options
-
+        System.out.println("Viewing Payslip..."); // Export View Payroll Options
     }
 }
