fa1d8bb21bd8027e7b145f7abd4b624b217d4ec2
FRENTYGOD1 <151792528+FRENTYGOD1@users.noreply.github.com>
2024-12-02 04:08:55 +0000

    Add files via upload

---
+++ UL Payroll System/src/EmployeeUser.java
@@ -0,0 +1,17 @@
+package users;
+
+public class EmployeeUser {
+    private String username;
+
+    public EmployeeUser(String username) {
+        this.username = username;//Set employee's username
+    }
+
+
+    //Employee User Menu
+    public void menu() {
+        System.out.println("Employee Menu:");
+        System.out.println("Viewing Payslip...");//Export View Payroll Options
+
+    }
+}
